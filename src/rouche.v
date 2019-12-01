77bfccf83b8c650adad88fa4ae45f1b199c7b00b
zach <zstonex@gmail.com>
2019-12-01 13:32:17 -0500

    working on applying Schwarz

--- src/rouche.v
+++ src/rouche.v
@@ -150,6 +150,10 @@ Section Holo.
   Definition Holo (f g: C -> C) z := 
     is_derive (K:=C_AbsRing) (V:= C_NormedModule) f z (g z).
 
+  Definition is_Holo (f: C -> C) z := 
+    exists g, Holo f g z.
+
+
   Lemma local_c_reals:
     forall (z:C) P,
       (within (fun q => q.2 = z.2) (locally (T:=C_UniformSpace) z)) P ->
@@ -261,6 +265,27 @@ Section Holo.
     ];
     [auto | apply filterdiff_fst | auto | apply filterdiff_snd].
   Qed.
+
+  Lemma holo_partials: forall u v g z, 
+    locally z (Holo (fun q => (u q, v q)) g) -> exists udx udy vdx vdy,
+      CR_eqs u v udx udy vdx vdy z . 
+  Proof.
+    move => u v g z [eps H].
+    eexists ?[udx].
+    eexists ?[udy].
+    eexists ?[vdx].
+    eexists ?[vdy].
+    rewrite /CR_eqs.
+    rewrite 4!prod_c_topology_eq.
+    repeat split.
+    - 
+    
+    
+    exists eps => y byz.
+    - apply c_diff_real_axis.
+    
+
+    rewrite -is_derive_pair_iff.
 
   Theorem CauchyRieman_Easy: forall u v udx udy vdx vdy g z,
     CR_eqs u v udx udy vdx vdy z -> 
@@ -703,50 +728,104 @@ Section CauchyIntegral.
 4: apply fn_eq_Derive_eq
 *)
 
-Definition SmoothPath (f: R -> R -> R) f_x f_y f_xy f_yx x y :=  
-  locally_2d
-  (fun u v : R =>
-    is_derive (f^~ v) u (f_x u v) /\
-    is_derive [eta f u] v (f_y u v) /\
-    is_derive (fun z : R => f_y z v) u (f_yx u v) /\
-    is_derive (fun z : R => f_x u z) v (f_xy u v) 
-  ) x y /\
-   continuity_2d_pt
-     (fun u v : R => Derive (fun z : R => Derive [eta f z] v) u) x y /\
-   continuity_2d_pt
-     (fun u : R => [eta Derive (fun z : R => Derive (f^~ z) u)]) x y 
-  .
-
 Open Scope R.
-
-Lemma differentiable_pt_lim_dirs: forall f a b dx dy, 
-  differentiable_pt_lim f a b dx dy -> 
-    is_derive ( fun x => f x b) a dx /\
-    is_derive ( f a) b dy.
+(** A stupid issue with the existing proof in Coquelicot.
+they prove Derives = _, but that's not strong enough.
+So I copy pasted the entire proof, minus one rewrite at the beginning*)
+Lemma differentiable_pt_lim_is_derive (f : R -> R -> R) (x y : R) (lx ly : R) :
+  differentiable_pt_lim f x y lx ly
+    -> is_derive (fun x => f x y) x lx /\ is_derive (fun y => f x y) y ly.
 Proof.
-  move => f a b dx dy.
-Abort.
+  move => Df ; split ; apply is_derive_Reals => e He ;
+  case: (Df (pos_div_2 (mkposreal e He))) => {Df} delta /= Df ;
+  exists delta => h Hh0 Hh.
 
-Lemma path_independence_part_1: 
-  forall 
+
+  replace ((f (x + h) y - f x y) / h - lx)
+    with ((f (x+h) y - f x y - (lx * ((x+h) - x) + ly * (y - y))) / h)
+    by (by field).
+  rewrite Rabs_div.
+  apply Rlt_div_l.
+  by apply Rabs_pos_lt.
+  apply Rle_lt_trans with (e / 2 * Rmax (Rabs (x + h - x)) (Rabs (y - y))).
+  apply (Df (x+h) y).
+  by (ring_simplify (x + h - x)).
+  rewrite Rminus_eq_0 Rabs_R0 ; by apply delta.
+  ring_simplify (x + h - x).
+  rewrite Rmax_left.
+  apply Rmult_lt_compat_r.
+  by apply Rabs_pos_lt.
+  lra.
+  rewrite Rminus_eq_0 Rabs_R0 ; by apply Rabs_pos.
+  by [].
+
+  replace ((f x (y + h) - f x y) / h - ly)
+    with ((f x (y + h) - f x y - (lx * (x - x) + ly * ((y + h) - y))) / h)
+    by (by field).
+  rewrite Rabs_div.
+  apply Rlt_div_l.
+  by apply Rabs_pos_lt.
+  apply Rle_lt_trans with (e / 2 * Rmax (Rabs (x - x)) (Rabs (y + h - y))).
+  apply (Df x (y + h)).
+  rewrite Rminus_eq_0 Rabs_R0 ; by apply delta.
+  by (ring_simplify (y + h - y)).
+  ring_simplify (y + h - y).
+  rewrite Rmax_right.
+  apply Rmult_lt_compat_r.
+  by apply Rabs_pos_lt.
+  lra.
+  rewrite Rminus_eq_0 Rabs_R0 ; by apply Rabs_pos.
+  by [].
+Qed.
+
+Lemma differentiable_pt_lim_left (f : R -> R -> R) (x y : R) (lx ly : R) :
+  differentiable_pt_lim f x y lx ly
+    -> ex_derive (fun x => f x y) x.
+Proof.
+  move => df.
+  have := (differentiable_pt_lim_is_derive df).
+  case => H _.
+  exists lx; auto.
+Qed.
+
+Lemma differentiable_pt_lim_right (f : R -> R -> R) (x y : R) (lx ly : R) :
+  differentiable_pt_lim f x y lx ly
+    -> ex_derive (fun y => f x y) y.
+Proof.
+  move => df.
+  have := (differentiable_pt_lim_is_derive df).
+  case => _ H.
+  exists ly; auto.
+Qed.
+
+
+Lemma path_independence_part_1: forall
     (u v: R -> R -> R) udx udy vdx vdy
     (g1 g2: R -> R -> R) g2_r g2_t g1_r g1_t r t,
   differentiable_pt_lim u (g1 r t) (g2 r t) udx udy ->
   differentiable_pt_lim v (g1 r t) (g2 r t) vdx vdy ->
   differentiable_pt_lim g1 r t (g1_r r t) (g1_t r t) ->
   differentiable_pt_lim g2 r t (g2_r r t) (g2_t r t) ->
+  differentiable_pt g1_t r t  ->
+  differentiable_pt g1_r r t  ->
+  differentiable_pt g2_t r t  ->
+  differentiable_pt g2_r r t  ->
   Derive (g1_t ^~ t) r = Derive (g1_r r) t ->
   Derive (g2_t ^~ t) r = Derive (g2_r r) t ->
   vdx = - udy ->
-  Derive(fun t0 : R_AbsRing =>
+  Derive(fun t0 =>
    (u (g1 r t0) (g2 r t0) * g1_r r t0) - v (g1 r t0) (g2 r t0) * (g2_r r t0)) t
   = 
   Derive
-  (fun r0 : R_AbsRing =>
+  (fun r0  =>
    (u (g1 r0 t) (g2 r0 t) * g1_t r0 t - (v (g1 r0 t) (g2 r0 t) * g2_t r0 t))%R) r.
 Proof.
   move => u v udx udy vdx vdy g1 g2 g2_r g2_t g1_r g1_t r t.
-  move => du dv dg1 dg2 swapdiff1 swapdiff2 CR_eq//=.
+  move => du dv dg1 dg2 dg1t dg1r dg2t dg2r swapdiff1 swapdiff2 CR_eq//=.
+  move: dg1t => [? [? dg1t]].
+  move: dg1r => [? [? dg1r]].
+  move: dg2t => [? [? dg2t]].
+  move: dg2r => [? [? dg2r]].
   set p := RHS.
   have ug : ( differentiable_pt_lim (fun r t => u (g1 r t) (g2 r t)) 
                 r t (udx * (g1_r r t) + udy * (g2_r r t))
@@ -762,11 +841,19 @@ Proof.
   `Begin eq p. { rewrite {}/p.
 
   | {(   Derive _ r - Derive _ r   )} rewrite ?Derive_minus.
-    1-2: shelve. 
+    apply ex_derive_mult.
+    apply (differentiable_pt_lim_left ug).
+    apply (differentiable_pt_lim_left dg1t).
+    apply ex_derive_mult.
+    apply (differentiable_pt_lim_left vg).
+    apply (differentiable_pt_lim_left dg2t).
   | {(   Derive _ r * _ + _ * Derive _ r 
          - (Derive _ r * _ + _ * Derive _ r)
     )}  rewrite ?Derive_mult.
-    1-4:shelve.
+    apply (differentiable_pt_lim_left vg).
+    apply (differentiable_pt_lim_left dg2t).
+    apply (differentiable_pt_lim_left ug).
+    apply (differentiable_pt_lim_left dg1t).
   | {(  (udx * (g1_r r t) + udy * (g2_r r t)) * _ + _ - _ )%R}
     do 2 apply Rplus_eq_compat_r;
     apply Rmult_eq_compat_r.
@@ -794,11 +881,19 @@ Proof.
   `Begin eq q. { rewrite {}/q.
 
   | {(   Derive _ _ - Derive _ _   )} rewrite ?Derive_minus.
-    1-2: shelve. 
+    apply ex_derive_mult.
+    apply (differentiable_pt_lim_right ug).
+    apply (differentiable_pt_lim_right dg1r).
+    apply ex_derive_mult.
+    apply (differentiable_pt_lim_right vg).
+    apply (differentiable_pt_lim_right dg2r).
   | {(   Derive _ _ * _ + _ * Derive _ _ 
          - (Derive _ _ * _ + _ * Derive _ _)
     )}  rewrite ?Derive_mult.
-    1-4:shelve.
+    apply (differentiable_pt_lim_right vg).
+    apply (differentiable_pt_lim_right dg2r).
+    apply (differentiable_pt_lim_right ug).
+    apply (differentiable_pt_lim_right dg1r).
   | {(  (udx * (g1_t r t) + udy * (g2_t r t)) * _ + _ - _ )%R}
     do 2 apply Rplus_eq_compat_r;
     apply Rmult_eq_compat_r.
@@ -825,81 +920,76 @@ Proof.
   rewrite swapdiff1 swapdiff2 ?CR_eq.
   lra.
 Unshelve.
-
-2: {
-match goal with 
-  | H: differentiable_pt_lim ?f ?x ?y ?dx ?dy |- 
-    ex_derive (?f ?x) ?y => idtac
-  end.
-
-  }
+2: apply ug.
+2: apply vg.
+2: apply ug.
+2: apply vg.
 Qed.
-  apply Rplus_eq_compat_r.
-  rewrite ?Rplus_assoc.
-  apply Rplus_eq_compat_l.
-  rewrite -?Rplus_assoc.
-  apply Rplus_eq_compat_r.
-  apply Rplus_eq_compat_r.
 
-  field_simplify.
-  rewrite /Rminus.
-  rewrite Rplus_comm.
-  rewrite swapdiff.
-  lra.
-  lra.
-  | {(  _ - ((vdx * g1_r r t + vdy * (g2_r r t)) * _ + _) )%R}
-    rewrite ?Rplus_assoc;
-    apply Rplus_eq_compat_l;
-    apply Ropp_eq_compat;
-    apply Rplus_eq_compat_r;
-    apply Rmult_eq_compat_r;
-    idtac .
-    pose hv := fun r' t' => v (g1 r' t') (g2 r' t').
-    under Derive_ext => r'.
-      rewrite (_: v _ _ = hv r' t); auto.
-    over.
-    eapply (differentiable_pt_lim_unique _ _ _ _ _ _).
-  `Done.
-  }
-    apply.
-    apply.
-    apply differentiable_pt_lim_unique .
-  | 
-  }
+Lemma path_independence_part_2:
+  forall (u v: R -> R -> R) r t g1 g2, 
+  let g:= fun p q => (g1 p q, g2 p q) in
+  let f:= fun z => (u z.1 z.2, v z.1 z.2) in
 
-  rewrite ?Derive_minus; 
-    try apply ex_derive_mult;
-  rewrite ?Derive_mult.
-  have: Derive (g1_r r) t = Derive (g1_t^~t) r. {
-    under Derive_ext => t0.
-    over.
-    admit.
-    (*apply Schwarz.*)
-  }
-  move=> ->.
-  set p := LHS.
-
+  (*smooth path*)
+  locally_2d (fun r' t' =>
+    ex_derive (fun z : R => g1 z t') r' /\
+    ex_derive (fun z : R => g1 r' z) t' /\
+    ex_derive (fun z : R => Derive (fun q => g1 z q) t') r' /\
+    ex_derive (fun z : R => Derive (fun q => g1 q z) r') t') r t ->
+  locally_2d (fun r' t' =>
+    ex_derive (fun z : R => g2 z t') r' /\
+    ex_derive (fun z : R => g2 r' z) t' /\
+    ex_derive (fun z : R => Derive (fun q => g2 z q) t') r' /\
+    ex_derive (fun z : R => Derive (fun q => g2 q z) r') t') r t ->
+  continuity_2d_pt (fun u v => Derive (fun z => Derive (fun t => g1 z t) v) u) r t ->
+  continuity_2d_pt (fun u v => Derive (fun z => Derive (fun t => g1 t z) u) v) r t ->
+  continuity_2d_pt (fun u v => Derive (fun z => Derive (fun t => g2 z t) v) u) r t ->
+  continuity_2d_pt (fun u v => Derive (fun z => Derive (fun t => g2 t z) u) v) r t ->
   
-  rewrite [LHS]Rplus_comm.
-  rewrite [RHS]Rplus_comm.
-  rewrite -?Rplus_assoc.
-  apply: Rplus_eq_compat_r.
-
-  under ext_is_derive_glo => y.
-    set p := _ * _.
-    simplify_as_R2 e p.
+  is_Holo f (g r t) -> 
+  let g_t := fun p q => (Derive (g1 p) q, Derive (g2 p) q) in
+  let g_r := fun p q => (Derive (g1 ^~ q) p, Derive (g2 ^~ q) p) in
+  Derive ( fun t0 => Re (f (g r t0) * g_r r t0 ))%C t =
+  Derive ( fun r0 => Re (f (g r0 t) * g_t r0 t ))%C r  
+.
+Proof.
+  move => u v r t g1 g2 g f g1_smooth g2_smooth .
+  move => g1_rt_cts g1_tt_cts g2_rt_cts g2_tr_cts.
+  move => g_t g_r.
+  simpl.
+  pose g1_r p q := Derive (g1 ^~ q) p.
+  pose g2_r p q := Derive (g2 ^~ q) p.
+  pose g1_t p q := Derive (g1 p) q .
+  pose g2_t p q := Derive (g2 p) q .
+  under Derive_ext => t0.
+    set p := Derive _ _ .
+    replace p with (g1_r r t0). 
+    set q := Derive _ _ .
+    replace q with (g2_r r t0). 
   over.
+  rewrite /p  //= .
+  rewrite /p  //= .
   symmetry.
-  under ext_is_derive_glo => y.
-    set p := _ * _.
-    simplify_as_R2 e p.
+  under Derive_ext => r0.
+    set p := Derive _ _ .
+    replace p with (g1_t r0 t). 
+    set q := Derive _ _ .
+    replace q with (g2_t r0 t). 
   over.
-  rewrite -?is_derive_pair_iff.
-  split.
+  rewrite /p  //= .
+  rewrite /p  //= .
+  symmetry.
+  eapply (@path_independence_part_1 u v _ _ _ _ g1 g2 _ _ _ _ r t).
+  9: rewrite /g1_t /g1_r; apply Schwarz; auto.
+  9: rewrite /g2_t /g2_r; apply Schwarz; auto.
+  
+  
+  
 
-  apply/is_derive_pair. in H.
-  Check Derive_correct.
-  pose p
+  Locally_
+
+Proof.
 
 
 
